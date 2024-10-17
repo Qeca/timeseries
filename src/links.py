0bab59cc0269816468fd4ed86aad3b110eb011ca
TemikkT <tanin04@mail.ru>
2024-10-17 14:33:45 +0300

    Добавление скриптов

---
+++ src/links.py
@@ -0,0 +1,35 @@
+links2024 = ['https://www.rbc.ru/search/?query=&project=quote&dateFrom=08.09.2024&dateTo=08.10.2024',
+         'https://www.rbc.ru/search/?query=&project=quote&dateFrom=08.08.2024&dateTo=07.09.2024',
+         'https://www.rbc.ru/search/?query=&project=quote&dateFrom=08.07.2024&dateTo=07.08.2024',
+         'https://www.rbc.ru/search/?query=&project=quote&dateFrom=08.06.2024&dateTo=07.07.2024',
+         'https://www.rbc.ru/search/?query=&project=quote&dateFrom=08.05.2024&dateTo=07.06.2024',
+         'https://www.rbc.ru/search/?query=&project=quote&dateFrom=08.04.2024&dateTo=07.05.2024',
+         'https://www.rbc.ru/search/?query=&project=quote&dateFrom=08.03.2024&dateTo=07.04.2024',
+         'https://www.rbc.ru/search/?query=&project=quote&dateFrom=08.02.2024&dateTo=07.03.2024',
+         'https://www.rbc.ru/search/?query=&project=quote&dateFrom=08.01.2024&dateTo=07.02.2024']
+
+
+links2023 = ['https://www.rbc.ru/search/?query=&project=quote&dateFrom=07.12.2023&dateTo=08.01.2024',
+         'https://www.rbc.ru/search/?query=&project=quote&dateFrom=07.11.2023&dateTo=06.12.2023',
+         'https://www.rbc.ru/search/?query=&project=quote&dateFrom=07.10.2023&dateTo=06.11.2023',
+         'https://www.rbc.ru/search/?query=&project=quote&dateFrom=07.09.2023&dateTo=06.10.2023',
+         'https://www.rbc.ru/search/?query=&project=quote&dateFrom=07.08.2023&dateTo=06.09.2023',
+         'https://www.rbc.ru/search/?query=&project=quote&dateFrom=07.07.2023&dateTo=06.08.2023',
+         'https://www.rbc.ru/search/?query=&project=quote&dateFrom=07.06.2023&dateTo=06.07.2023',
+         'https://www.rbc.ru/search/?query=&project=quote&dateFrom=07.05.2023&dateTo=06.06.2023',
+         'https://www.rbc.ru/search/?query=&project=quote&dateFrom=07.04.2023&dateTo=06.05.2023',
+         'https://www.rbc.ru/search/?query=&project=quote&dateFrom=07.03.2023&dateTo=06.04.2023',
+         'https://www.rbc.ru/search/?query=&project=quote&dateFrom=07.02.2023&dateTo=06.03.2023',
+         'https://www.rbc.ru/search/?query=&project=quote&dateFrom=07.01.2023&dateTo=06.02.2023']
+
+
+links2022 = ['https://www.rbc.ru/search/?query=&project=quote&dateFrom=07.12.2022&dateTo=06.01.2023',
+         'https://www.rbc.ru/search/?query=&project=quote&dateFrom=07.11.2022&dateTo=06.12.2022',
+         'https://www.rbc.ru/search/?query=&project=quote&dateFrom=07.10.2022&dateTo=06.11.2022',
+         'https://www.rbc.ru/search/?query=&project=quote&dateFrom=07.09.2022&dateTo=06.10.2022',
+         'https://www.rbc.ru/search/?query=&project=quote&dateFrom=07.08.2022&dateTo=06.09.2022',
+         'https://www.rbc.ru/search/?query=&project=quote&dateFrom=07.07.2022&dateTo=06.08.2022',
+         'https://www.rbc.ru/search/?query=&project=quote&dateFrom=07.06.2022&dateTo=06.07.2022',
+         'https://www.rbc.ru/search/?query=&project=quote&dateFrom=07.05.2022&dateTo=06.06.2022',
+         'https://www.rbc.ru/search/?query=&project=quote&dateFrom=07.04.2022&dateTo=06.05.2022',
+         'https://www.rbc.ru/search/?query=&project=quote&dateFrom=07.03.2022&dateTo=06.04.2022']
